2a6e2e1ef65a9d315513f01da4e76e245fc998b6
jambanna-123 <166196326+jambanna-123@users.noreply.github.com>
2024-08-12 11:27:31 +0530

    Update DemoWebShopTest.java

--- SeleniumProject/src/test/java/demo/DemoWebShopTest.java
+++ SeleniumProject/src/test/java/demo/DemoWebShopTest.java
@@ -20,8 +20,7 @@ void test() {
 	driver.findElement(By.linkText("Log in")).click();
 	driver.findElement(By.id("Email")).sendKeys(email);
 	driver.findElement(By.id("Password")).sendKeys(password);
-	driver.findElement(By.xpath("//input[@value='Log in']")).click();
-	driver.findElement(By.xpath("//input[@value='Log in']")).click();
+	driver.findElement(By.xpath("//input[@value='Log in']")).click(
 	
 }
 }
